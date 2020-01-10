648dad8af3ffd096633374303b7b3448db688cdd
Jus de Patate_ <jusdepatate@protonmail.com>
2020-01-10 14:23:28 +0100

    curl is not required now

--- check.php
+++ check.php
@@ -5,13 +5,6 @@ $nl = nl();
 init_html();
 ascii_logo();
 
-if (extension_loaded("curl")) {
-    echo "php's curl is loaded$nl";
-    $curl=True;
-} else {
-    echo "Please install php's curl$nl";
-    $curl=False;
-}
 if (extension_loaded("sqlite3")) {
     echo "SQLite3 is loaded$nl";
     $sqlite=True;
@@ -26,7 +19,7 @@ if (is_writable(".")) {
     echo "folder is not writeable$nl";
     $writeable=False;
 }
-if ($writeable && $sqlite && $curl) {
+if ($writeable && $sqlite) {
     echo "Everything's OK ! You are ready to run this TWTXT Registry$nl";
 }
 
