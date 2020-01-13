07dc79adaec5780222b8e215d7eafa1c188b4d91
Jus de Patate_ <jusdepatate@protonmail.com>
2020-01-13 08:22:23 +0100

    add search in the db with lists.php

--- list.php
+++ list.php
@@ -5,13 +5,36 @@ ban();
 $nl = nl();
 init_html();
 
-$db = new TWTXTdb();
-$result = $db->query('SELECT * FROM users');
+if (isset($_GET["q"])) {
+    # if the user set an argument ?q to url, search for the user
+    $q = strip_tags($_GET["q"]);
+    $q = sqlite3::escapeString($q);
+    # anti sql injection + anti xss (we never know)
 
-echo "user url$nl";
+    $db = new TWTXTdb();
+    $query = $db->prepare('SELECT * FROM users WHERE name = ?');
+    $query->bindParam(1, $q);
 
-while ($row = $result->fetchArray()) {
-    echo "{$row[0]} {$row[1]}$nl";
+    $res = $query->execute();
+    $row = $res->fetchArray();
+
+    if ($row) {
+        print_r("{$row[0]} {$row[1]}$nl");
+        # output the user we've found (will output only the first occurence)
+    } else {
+        header("Not Found", TRUE, 404);
+        echo $nl;
+        # reply 404 to client if no user found by db
+    }
+
+} else {
+    $db = new TWTXTdb();
+    $query = $db->query('SELECT * FROM users');
+
+    while ($row = $query->fetchArray()) {
+        echo "{$row[0]} {$row[1]}$nl";
+    }
 }
+$db->close();
 
 end_html();
